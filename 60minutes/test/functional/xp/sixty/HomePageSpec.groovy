4df5a41b4cd0173bf246ce746de32048530e3afa
Marcin Floryan <marcin.floryan@gmail.com>
2011-11-13 21:23:16 +0000

    tabs or spaces

--- 60minutes/test/functional/xp/sixty/HomePageSpec.groovy
+++ 60minutes/test/functional/xp/sixty/HomePageSpec.groovy
@@ -4,15 +4,15 @@ import geb.spock.GebSpec
 
 class HomePageSpec extends GebSpec  {
 
-	def "Should show empty home page"() {
+    def "Should show empty home page"() {
 
-		given: "An empty app"
-        when: "I open the application home page"
+    given: "An empty app"
+    when: "I open the application home page"
 
-		go "/60minutes/"
+        go "/60minutes/"
 
-		then: "I see an empty page"
+    then: "I see an empty page"
 
-		title == "60 minutes"
-	}
+        title == "60 minutes"
+    }
 }
